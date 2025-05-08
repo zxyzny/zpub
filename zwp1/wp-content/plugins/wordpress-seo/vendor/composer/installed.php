363ba993ca00793e857d054436c4ce911b884a56
zxyzny <nima_yeganeh@msn.com>
2025-05-08 12:13:50 +0000

    done

---
+++ zwp1/wp-content/plugins/wordpress-seo/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e01ec0c158abf0a450f9298079820f0b36c7da5f',
+        'name' => 'yoast/wordpress-seo',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+            'dev_requirement' => false,
+        ),
+        'yoast/wordpress-seo' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e01ec0c158abf0a450f9298079820f0b36c7da5f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
