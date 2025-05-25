d04185b93093f7804f6b44a82803a61c957d599d
zxyzny <nima_yeganeh@msn.com>
2025-05-25 06:09:58 +0000

    done

--- zchownconfig.sh
+++ zchownconfig.sh
@@ -1,5 +1,37 @@
-ls -lh /root/zarthistweb
-sudo chown -R www-data:www-data /root/zarthistweb/
-sudo chown www-data:www-data /root/zarthistweb/
-ls -lh /root/zarthistweb
+
+ls -lh /root/zpub/ | grep zwp
+sudo chown -R www-data:www-data /root/zpub/zwp1
+sudo chown www-data:www-data /root/zpub/zwp1
+sudo chown -R www-data:www-data /root/zpub/zwp2
+sudo chown www-data:www-data /root/zpub/zwp2
+sudo chown -R www-data:www-data /root/zpub/zwp3
+sudo chown www-data:www-data /root/zpub/zwp3
+sudo chown -R www-data:www-data /root/zpub/zwp4
+sudo chown www-data:www-data /root/zpub/zwp4
+sudo chown -R www-data:www-data /root/zpub/zwp5
+sudo chown www-data:www-data /root/zpub/zwp5
+sudo chown -R www-data:www-data /root/zpub/zwp6
+sudo chown www-data:www-data /root/zpub/zwp6
+sudo chown -R www-data:www-data /root/zpub/zwp7
+sudo chown www-data:www-data /root/zpub/zwp7
+sudo chown -R www-data:www-data /root/zpub/zwp8
+sudo chown www-data:www-data /root/zpub/zwp8
+sudo chown -R www-data:www-data /root/zpub/zwp9
+sudo chown www-data:www-data /root/zpub/zwp9
+sudo chown -R www-data:www-data /root/zpub/zwp10
+sudo chown www-data:www-data /root/zpub/zwp10
+sudo chown -R www-data:www-data /root/zpub/zwp11
+sudo chown www-data:www-data /root/zpub/zwp11
+sudo chown -R www-data:www-data /root/zpub/zwp12
+sudo chown www-data:www-data /root/zpub/zwp12
+sudo chown -R www-data:www-data /root/zpub/zwp13
+sudo chown www-data:www-data /root/zpub/zwp13
+sudo chown -R www-data:www-data /root/zpub/zwp14
+sudo chown www-data:www-data /root/zpub/zwp14
+sudo chown -R www-data:www-data /root/zpub/zwp15
+sudo chown www-data:www-data /root/zpub/zwp15
+sudo chown -R www-data:www-data /root/zpub/zwp16
+sudo chown www-data:www-data /root/zpub/zwp16
+ls -lh /root/zpub/ | grep zwp
+
 
