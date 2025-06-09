68b014db362a7e5e89f3d836cb5450566870ce5b
zxyzny <nima_yeganeh@msn.com>
2025-06-09 09:16:30 +0000

    done

--- zrdpx.sh
+++ zrdpx.sh
@@ -8,6 +8,13 @@ sudo systemctl status xrdp
 sudo ufw allow 3389/tcp
 sudo snap install firefox
 
+sudo apt update -y
+sudo apt install xfce4 -y
+echo "startxfce4" > ~/.xsession
+sudo systemctl restart xrdp
+
+
+
 
 
 
